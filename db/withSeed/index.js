d3cf6f27a95216f359168e2884ab3a23f3ba4cdb
Steve <bevets87@gmail.com>
2019-01-03 21:38:29 -0500

    removed seed in production mode

--- db/withSeed/index.js
+++ db/withSeed/index.js
@@ -2,15 +2,21 @@ const seed = require("./seed");
 const queries = require("./queries");
 
 module.exports = Database => {
-  Database.prototype.seed = function(options) {
-    const { users, polls, choices, votes } = seed(options);
-    return this.query(queries.users(), [users])
-      .then(() => this.query(queries.polls(), [polls]))
-      .then(() =>
-        Promise.all(choices.map(c => this.query(queries.choices(), [c])))
-      )
-      .then(() => this.query(queries.votes(), [votes]));
-  };
+  if (process.env.NODE_ENV === "development") {
+    Database.prototype.seed = function(options) {
+      const { users, polls, choices, votes } = seed(options);
+      return this.query(queries.users(), [users])
+        .then(() => this.query(queries.polls(), [polls]))
+        .then(() =>
+          Promise.all(choices.map(c => this.query(queries.choices(), [c])))
+        )
+        .then(() => this.query(queries.votes(), [votes]));
+    };
+  } else {
+    Database.prototype.seed = function(options) {
+      return Promise.resolve(options);
+    };
+  }
 
   return Database;
 };
